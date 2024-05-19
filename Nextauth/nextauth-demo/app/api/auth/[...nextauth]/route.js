f4a22723cfd46e4f1f360f0b31489f426b321cb9
Anideep <anideepkalia@gmail.com>
2024-05-19 06:22:14 +0530

    syntax fixed

--- Nextauth/nextauth-demo/app/api/auth/[...nextauth]/route.js
+++ Nextauth/nextauth-demo/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,9 @@
 import NextAuth from 'next-auth'
 import GithubProvider from "next-auth/providers/github"
 
+
+// if using src then your folder looks like: app/api/auth/[...nextauth]/route.js
+
 const handler = NextAuth({
   providers: [
     // OAuth authentication providers...
